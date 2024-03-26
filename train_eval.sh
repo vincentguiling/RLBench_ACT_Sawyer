6d476719e2caac0956fabb47c6a8f6e240383234
boxjod <boxjod@yahoo.com>
2024-03-26 20:19:02 +0800

    bash

--- train_eval.sh
+++ train_eval.sh
@@ -1,7 +1,7 @@
-# #!/bin/bash
-# conda activate xj_rlbench
-# git fetch
-# git pull
+#!/bin/bash
+conda activate xj_rlbench
+git fetch
+git pull
 
 # CUDA_VISIBLE_DEVICES=7 python3 act/imitate_episodes_sawyer2.py \
 # --task_name reach_target_sawyer \
@@ -32,21 +32,21 @@ for epoch in ${epoch_list[@]}
   echo '#######################################'
   
   
-  # CUDA_VISIBLE_DEVICES=7 python3 act/imitate_episodes_sawyer2.py \
-  # --task_name reach_target_sawyer \
-  # --ckpt_dir Trainings \
-  # --policy_class ACT --kl_weight 10 --chunk_size 10 --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
-  # --num_epochs $epoch  --lr 1e-5 \
-  # --seed 0 \
-  # ; \
-  # python3 act/imitate_episodes_sawyer2.py \
-  # --task_name reach_target_sawyer \
-  # --ckpt_dir Trainings \
-  # --policy_class ACT --kl_weight 10 --chunk_size 10 --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
-  # --num_epochs 1000  --lr 1e-5 \
-  # --seed 0 \
-  # --eval \
-  # --temporal_agg 
+  CUDA_VISIBLE_DEVICES=7 python3 act/imitate_episodes_sawyer2.py \
+  --task_name reach_target_sawyer \
+  --ckpt_dir Trainings \
+  --policy_class ACT --kl_weight 10 --chunk_size 10 --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
+  --num_epochs $epoch  --lr 1e-5 \
+  --seed 0 \
+  ; \
+  CUDA_VISIBLE_DEVICES=7 python3 act/imitate_episodes_sawyer2.py \
+  --task_name reach_target_sawyer \
+  --ckpt_dir Trainings \
+  --policy_class ACT --kl_weight 10 --chunk_size 10 --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
+  --num_epochs $epoch  --lr 1e-5 \
+  --seed 0 \
+  --eval \
+  --temporal_agg 
   
   
   done
