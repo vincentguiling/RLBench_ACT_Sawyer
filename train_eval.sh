52d69bdffacb52442c099b06cf8a23a0829a0b7e
boxjod <boxjod@yahoo.com>
2024-03-26 18:59:53 +0800

    cmd

--- train_eval.sh
+++ train_eval.sh
@@ -3,7 +3,7 @@ conda activate xj_rlbench
 git fetch
 git pull
 
-CUDA_VISIBLE_DEVICES=-1 python3 act/imitate_episodes_sawyer2.py \
+CUDA_VISIBLE_DEVICES=7 python3 act/imitate_episodes_sawyer2.py \
 --task_name reach_target_sawyer \
 --ckpt_dir Trainings/train \
 --policy_class ACT --kl_weight 10 --chunk_size 10 --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
