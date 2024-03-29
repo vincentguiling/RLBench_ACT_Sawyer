e0b4de6ecb9d87206509b71d47cbedc77b312a23
boxjod <boxjod@yahoo.com>
2024-03-29 22:36:42 +0800

    11

--- train_eval.sh
+++ train_eval.sh
@@ -19,14 +19,14 @@ for batch in ${batch_size[@]}
     echo '###########################################################'
     
     CUDA_VISIBLE_DEVICES=7 python3 act/imitate_episodes_sawyer2.py \
-    --task_name reach_target_sawyer2 \
+    --task_name reach_target_sawyer4 \
     --ckpt_dir Trainings \
     --policy_class ACT --kl_weight 10 --chunk_size 10 --hidden_dim 512 --batch_size $batch --dim_feedforward 3200 \
     --num_epochs $epoch  --lr 1e-5 \
     --seed 0 \
     ; \
     CUDA_VISIBLE_DEVICES=7 python3 act/imitate_episodes_sawyer2.py \
-    --task_name reach_target_sawyer2 \
+    --task_name reach_target_sawyer4 \
     --ckpt_dir Trainings \
     --policy_class ACT --kl_weight 10 --chunk_size 10 --hidden_dim 512 --batch_size $batch --dim_feedforward 3200 \
     --num_epochs $epoch  --lr 1e-5 \
