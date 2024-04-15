68728eead2fb2787fd59021cf99986927d1661c4
boxjod <boxjod@yahoo.com>
2024-04-15 10:02:51 +0800

    dp_server

--- train_eval.sh
+++ train_eval.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 生产新的数据，里面没有nolinear
-python3 RLBench/tools/dataset_generator_sawyer_act3.py \
-    --save_path Datasets \
-    --tasks sorting_program21 \
-    --variations 2 \
-    --episodes_per_task 50 \
+# python3 RLBench/tools/dataset_generator_sawyer_act3.py \
+#     --save_path Datasets \
+#     --tasks sorting_program21 \
+#     --variations 2 \
+#     --episodes_per_task 50 \
     
 epoch_list=(1000 2000 3000)
 batch_size=(4 8 16 32)
@@ -29,12 +29,14 @@ for batch in ${batch_size[@]}
         --ckpt_dir Trainings \
         --policy_class ACT --kl_weight 10 --chunk_size $chunk --hidden_dim 512 --batch_size $batch --dim_feedforward 3200 \
         --num_epochs $epoch  --lr 1e-5 --seed 0 --backbone $backbone \
+        --use_language --language_encoder distilbert    \
         ; \
         CUDA_VISIBLE_DEVICES=3 python3 act2/imitate_episodes_sawyer4.py \
         --task_name sorting_program21 \
         --ckpt_dir Trainings \
         --policy_class ACT --kl_weight 10 --chunk_size $chunk --hidden_dim 512 --batch_size $batch --dim_feedforward 3200 \
         --num_epochs $epoch  --lr 1e-5 --seed 0 --backbone $backbone \
+        --use_language --language_encoder distilbert    \
         --eval --temporal_agg 
         done
       done
