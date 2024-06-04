dda1f8bd61a53f9e49f628c2a26fe060898e640d
boxjod <boxjod@yahoo.com>
2024-06-04 11:30:45 +0800

    secer

--- train_eval.sh
+++ train_eval.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # 生成数据集
-python3 RLBench/tools/dataset_generator_sawyer_act3.py \
-    --save_path Datasets \
-    --tasks sorting_program21 \
-    --variations 1 \
-    --episodes_per_task 50
+# python3 RLBench/tools/dataset_generator_sawyer_act3.py \
+#     --save_path Datasets \
+#     --tasks sorting_program21 \
+#     --variations 1 \
+#     --episodes_per_task 50
 
-model_type=(ACT0E0 ACT3E0 ACT3E2 ACT3E3)
-epoch_list=(1000 2000 3000 4000 5000)
+model_type=(ACT3E3)
+epoch_list=(2000 3000 4000 5000 6000 7000)
 backbone_list=("efficientnet_b0")
 chunk_size=(20)
 for model in ${model_type[@]}
@@ -23,13 +23,13 @@ for model in ${model_type[@]}
         echo 'train on model=' $model ', epoch=' $epoch ',  chunk_size='$chunk 
         echo '##################################################################'
         
-        CUDA_VISIBLE_DEVICES=4 python3 act2/imitate_episodes_sawyer4.py \
+        CUDA_VISIBLE_DEVICES=1 python3 act2/imitate_episodes_sawyer4.py \
         --task_name sorting_program21 \
         --ckpt_dir Trainings \
         --policy_class $model --kl_weight 10 --chunk_size $chunk --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
         --num_epochs $epoch  --lr 1e-5 --seed 0 --backbone $backbone \
         ; \
-        CUDA_VISIBLE_DEVICES=4 python3 act2/imitate_episodes_sawyer4.py \
+        CUDA_VISIBLE_DEVICES=1 python3 act2/imitate_episodes_sawyer4.py \
         --task_name sorting_program21 \
         --ckpt_dir Trainings \
         --policy_class $model --kl_weight 10 --chunk_size $chunk --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
